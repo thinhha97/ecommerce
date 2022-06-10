1829c5d8dc1d4ecdef8b23d56748b6a563d0aab3
Thinh Ha <havanthinh97@gmail.com>
2022-06-10 16:45:16 +0700

    create components.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 const index = () => {
   return (
-    <div></div>
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best selling products</h2>
+        <p>Style for women</p>
+      </div>
+      <div className="products-container">
+        {['product1', 'product2'].map((product) => product)}
+      </div>
+      Footer
+    </>
   )
 }
 

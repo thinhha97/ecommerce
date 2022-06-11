a49c58a993190ab7892c03ec07a90f9aaccc2c26
Thinh Ha <havanthinh97@gmail.com>
2022-06-11 21:44:59 +0700

    Implement 'HeroBanner' component.

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,21 +3,31 @@ import Link from 'next/link'
 
 import { urlFor } from '../lib/client'
 
-const HeroBanner = ({heroBanner}) => {
+const HeroBanner = ({
+  heroBanner: {
+    smallText,
+    midText,
+    largeText,
+    image,
+    product,
+    buttonText,
+    desc,
+  },
+}) => {
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
       <div>
-        <p className="styles-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText}</h1>
-        <img src={urlFor(heroBanner.image)} alt="styles" className='hero-banner-image' />
+        <p className="styles-solo">{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText}</h1>
+        <img src={urlFor(image)} alt="styles" className="hero-banner-image" />
         <div>
-          <Link href={`product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
+          <Link href={`product/${product}`}>
+            <button type="button">{buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <p>{desc}</p>
           </div>
         </div>
       </div>

040a8dee2c1f4169dd7e3ce8d23c6a44bdc8f7f5
Thinh Ha <havanthinh97@gmail.com>
2022-06-11 21:44:39 +0700

    Implement 'Footer' component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>&copy; 2022, Fashion X, Inc.</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+
+    </div>
   )
 }
 

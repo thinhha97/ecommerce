81830be98b3b1b3f315a440fb2cf04ec9c9d1a24
Thinh Ha <havanthinh97@gmail.com>
2022-06-16 18:59:11 +0700

    edit typo in property name.

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = ({ products, bannerData }) => {
       </div>
       <div className="products-container">
         {products?.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product key={product._id} product={product} />
         ))}
       </div>
       <FooterBanner footerBanner={bannerData && bannerData[0]} />

39453346c9d6909edd81e27d413fa1ddc3adfad1
Thinh Ha <havanthinh97@gmail.com>
2022-06-17 01:19:08 +0700

    implement functionallity for checkout btn

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,6 +11,7 @@ import Toast from 'react-hot-toast'
 
 import { useStateContext } from '../context/StateContext'
 import { urlFor } from '../lib/client'
+import getStripe from '../lib/getStripe'
 
 const Cart = () => {
   const cartRef = useRef()
@@ -20,8 +21,27 @@ const Cart = () => {
     cartItems,
     setShowCart,
     toggleCartItemQty,
-    onRemove
+    onRemove,
   } = useStateContext()
+  const handleCheckout = async () => {
+    const stripe = await getStripe()
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    })
+
+    if (response.statusCode === 500) return
+
+    const data = await response.json()
+
+    Toast.loading('Đang chuyển hướng đến trang thanh toán...')
+    console.log('DATA',data)
+    stripe.redirectToCheckout({ sessionId: data.id })
+  }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -67,7 +87,7 @@ const Cart = () => {
                         >
                           <AiOutlineMinus />
                         </span>
-                        <span className="num" onClick="">
+                        <span className="num" >
                           {item.quantity}
                         </span>
                         <span
@@ -78,7 +98,10 @@ const Cart = () => {
                         </span>
                       </p>
                     </div>
-                    <button className="remove-item" onClick={() => onRemove(item)}>
+                    <button
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
+                    >
                       <TiDeleteOutline />
                     </button>
                   </div>
@@ -93,7 +116,7 @@ const Cart = () => {
               <h3>₫{totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button className="btn" onClick="">
+              <button className="btn" onClick={handleCheckout}>
                 Thanh Toán
               </button>
             </div>

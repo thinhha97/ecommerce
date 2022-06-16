4d50b78254bf0d472921c9c3fcfd59e8b3b61e27
Thinh Ha <havanthinh97@gmail.com>
2022-06-16 20:23:43 +0700

    create 'Cart' component.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,106 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import {
+  AiOutlinePlus,
+  AiOutlineMinus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from 'react-icons/ai'
+import { TiDeleteOutline } from 'react-icons/ti'
+import Toast from 'react-hot-toast'
+
+import { useStateContext } from '../context/StateContext'
+import { urlFor } from '../lib/client'
 
 const Cart = () => {
+  const cartRef = useRef()
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQty,
+    onRemove
+  } = useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button className="cart-heading" onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className="heading">Giỏ hàng của bạn</span>
+          <span className="cart-num-items">({totalQuantities} sản phẩm)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Bạn hiện đang không có sản phẩm nào trong giỏ hàng.</h3>
+            <Link href="/">
+              <button onClick={() => setShowCart(false)} className="btn">
+                Tiếp tục mua sắm
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  alt=""
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>₫{item.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span
+                          className="minus"
+                          onClick={() => toggleCartItemQty(item._id, 'dec')}
+                        >
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick="">
+                          {item.quantity}
+                        </span>
+                        <span
+                          className="plus"
+                          onClick={() => toggleCartItemQty(item._id, 'inc')}
+                        >
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button className="remove-item" onClick={() => onRemove(item)}>
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Tổng: </h3>
+              <h3>₫{totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button className="btn" onClick="">
+                Thanh Toán
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 

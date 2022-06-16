848912b453176408a000befde4075879f2ca48f2
Thinh Ha <havanthinh97@gmail.com>
2022-06-16 18:59:34 +0700

    Create a StateContext for the whole app.

---
+++ context/StateContext.js
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { toast } from 'react-hot-toast'
+
+const Context = createContext()
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false)
+  const [cartItems, setCartItems] = useState([])
+  const [totalPrice, setTotalPrice] = useState()
+  const [totalQuantities, setTotalQuantities] = useState(0)
+  const [qty, setQty] = useState(1)
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    )
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+    if (checkProductInCart) {
+      const updateCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          }
+      })
+
+      setCartItems(updateCartItems)
+    } else {
+      product.quantity = quantity
+      setCartItems([...cartItems, { ...product }])
+    }
+    toast.success(
+      `${qty} sản phẩm "${product.name}"  đã được thêm vào giỏ hàng!`,
+      { duration: 2500 }
+    )
+  }
+
+  function increaseQty() {
+    setQty((prevQty) => prevQty + 1)
+  }
+
+  const decreaseQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) {
+        return 1
+      } else {
+        return prevQty - 1
+      }
+    })
+  }
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        increaseQty,
+        decreaseQty,
+        onAdd,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(Context)
